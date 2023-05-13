e298bae93939988ee6570591ab2b304d0c5058ec
Uriel de Oliveira Alves <uriel.oalves@gmail.com>
2023-05-13 19:31:22 +0200

    use ServerException in JwtService

--- src/main/kotlin/dev/urieloalves/services/JwtService.kt
+++ src/main/kotlin/dev/urieloalves/services/JwtService.kt
@@ -3,7 +3,7 @@ package dev.urieloalves.services
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import dev.urieloalves.configs.Env
-import dev.urieloalves.data.models.errors.CustomException
+import dev.urieloalves.data.models.errors.ServerException
 import java.time.Instant
 
 interface JwtService {
@@ -19,8 +19,7 @@ class JwtServiceImpl : JwtService {
                 .withExpiresAt(Instant.now().plusSeconds(Env.JWT_EXPIRES_IN_MINUTES * 60))
                 .sign(Algorithm.HMAC256(Env.JWT_SECRET))
         } catch (e: Exception) {
-            throw CustomException(
-                statusCode = 500,
+            throw ServerException(
                 message = "Could not generate JWT token for user `$userId`",
                 cause = e
             )
