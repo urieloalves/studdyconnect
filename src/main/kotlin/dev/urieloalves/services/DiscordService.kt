4a60b67749c02e6b15c61d81ae06f82e08cafe0b
Uriel de Oliveira Alves <uriel.oalves@gmail.com>
2023-05-13 19:05:38 +0200

    handle exceptions in DiscordServer

--- src/main/kotlin/dev/urieloalves/services/DiscordService.kt
+++ src/main/kotlin/dev/urieloalves/services/DiscordService.kt
@@ -10,6 +10,7 @@ import dev.kord.rest.service.editRolePermission
 import dev.urieloalves.clients.DiscordClient
 import dev.urieloalves.clients.responses.DiscordUserResponse
 import dev.urieloalves.configs.Env
+import dev.urieloalves.data.models.CustomException
 
 
 interface DiscordService {
@@ -24,71 +25,119 @@ interface DiscordService {
 class DiscordServiceImpl(
     val discordClient: DiscordClient
 ) : DiscordService {
-    
+
     override suspend fun getAccessToken(code: String): String {
-        return discordClient.getAccessToken(code)
+        try {
+            return discordClient.getAccessToken(code)
+        } catch (e: Exception) {
+            throw CustomException(
+                statusCode = 500,
+                message = "Could not get discord access token",
+                cause = e
+            )
+        }
     }
 
     override suspend fun getUser(accessToken: String): DiscordUserResponse {
-        return discordClient.getUser(accessToken)
+        try {
+            return discordClient.getUser(accessToken)
+        } catch (e: Exception) {
+            throw CustomException(
+                statusCode = 500,
+                message = "Could not get discord user",
+                cause = e
+            )
+        }
     }
 
     override suspend fun joinServer(discordId: String, token: String) {
-        val kord = Kord(Env.DISCORD_BOT_TOKEN)
+        try {
+            val kord = Kord(Env.DISCORD_BOT_TOKEN)
 
-        kord.rest.guild.addGuildMember(
-            guildId = Snowflake(Env.DISCORD_GUILD_ID),
-            userId = Snowflake(discordId),
-            token = token
-        ) {}
+            kord.rest.guild.addGuildMember(
+                guildId = Snowflake(Env.DISCORD_GUILD_ID),
+                userId = Snowflake(discordId),
+                token = token
+            ) {}
+        } catch (e: Exception) {
+            throw CustomException(
+                statusCode = 500,
+                message = "Could not add discord user '$discordId' to StudyConnect server",
+                cause = e
+            )
+        }
     }
 
     override suspend fun createChannel(name: String, description: String, discordId: String): String {
-        val kord = Kord(Env.DISCORD_BOT_TOKEN)
+        try {
+            val kord = Kord(Env.DISCORD_BOT_TOKEN)
 
-        val channel = kord.rest.guild.createTextChannel(
-            guildId = Snowflake(Env.DISCORD_GUILD_ID),
-            name = name
-        ) {
-            topic = description
+            val channel = kord.rest.guild.createTextChannel(
+                guildId = Snowflake(Env.DISCORD_GUILD_ID),
+                name = name
+            ) {
+                topic = description
+            }
+
+            kord.rest.channel.editRolePermission(
+                channelId = channel.id,
+                roleId = Snowflake(Env.DISCORD_ROLE_EVERYONE_ID)
+            ) {
+                denied = Permissions(Permission.All)
+            }
+
+            kord.rest.channel.editMemberPermissions(
+                channelId = channel.id,
+                memberId = Snowflake(discordId)
+            ) {
+                allowed = Permissions(Permission.All)
+            }
+
+            return channel.id.toString()
+        } catch (e: Exception) {
+            throw CustomException(
+                statusCode = 500,
+                message = "Could not create channel for discord user '$discordId'",
+                cause = e
+            )
         }
-
-        kord.rest.channel.editRolePermission(
-            channelId = channel.id,
-            roleId = Snowflake(Env.DISCORD_ROLE_EVERYONE_ID)
-        ) {
-            denied = Permissions(Permission.All)
-        }
-
-        kord.rest.channel.editMemberPermissions(
-            channelId = channel.id,
-            memberId = Snowflake(discordId)
-        ) {
-            allowed = Permissions(Permission.All)
-        }
-
-        return channel.id.toString()
     }
 
     override suspend fun joinChannel(channelId: String, discordId: String) {
-        val kord = Kord(Env.DISCORD_BOT_TOKEN)
+        try {
+            val kord = Kord(Env.DISCORD_BOT_TOKEN)
 
-        kord.rest.channel.editMemberPermissions(
-            channelId = Snowflake(channelId),
-            memberId = Snowflake(discordId)
-        ) {
-            allowed = Permissions(Permission.All)
+            kord.rest.channel.editMemberPermissions(
+                channelId = Snowflake(channelId),
+                memberId = Snowflake(discordId)
+            ) {
+                allowed = Permissions(Permission.All)
+            }
+        } catch (e: Exception) {
+            throw CustomException(
+                statusCode = 500,
+                message = "Could not add discord user '$discordId' to channel '$channelId'",
+                cause = e
+            )
         }
     }
 
     override suspend fun leaveChannel(channelId: String, discordId: String) {
-        val kord = Kord(Env.DISCORD_BOT_TOKEN)
+        try {
+            val kord = Kord(Env.DISCORD_BOT_TOKEN)
 
-        kord.rest.channel.editMemberPermissions(
-            channelId = Snowflake(channelId),
-            memberId = Snowflake(discordId)
-        ) {
-            denied = Permissions(Permission.All)
+            kord.rest.channel.editMemberPermissions(
+                channelId = Snowflake(channelId),
+                memberId = Snowflake(discordId)
+            ) {
+                denied = Permissions(Permission.All)
+            }
+        } catch (e: Exception) {
+            throw CustomException(
+                statusCode = 500,
+                message = "Could not remove discord user `$discordId` from channel '$channelId'",
+                cause = e
+            )
         }
     }
 }
