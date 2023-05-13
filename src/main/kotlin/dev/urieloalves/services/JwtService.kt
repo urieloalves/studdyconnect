75e4d43e9dd089bc139b0dab10b011d0b2e59904
Uriel de Oliveira Alves <uriel.oalves@gmail.com>
2023-05-13 19:08:29 +0200

    handle exceptions in JwtService

--- src/main/kotlin/dev/urieloalves/services/JwtService.kt
+++ src/main/kotlin/dev/urieloalves/services/JwtService.kt
@@ -3,6 +3,7 @@ package dev.urieloalves.services
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import dev.urieloalves.configs.Env
+import dev.urieloalves.data.models.CustomException
 import java.time.Instant
 
 interface JwtService {
@@ -12,9 +13,18 @@ interface JwtService {
 class JwtServiceImpl : JwtService {
 
     override fun generateToken(userId: String): String {
-        return JWT.create()
-            .withClaim("id", userId)
-            .withExpiresAt(Instant.now().plusSeconds(Env.JWT_EXPIRES_IN_MINUTES * 60))
-            .sign(Algorithm.HMAC256(Env.JWT_SECRET))
+        try {
+            return JWT.create()
+                .withClaim("id", userId)
+                .withExpiresAt(Instant.now().plusSeconds(Env.JWT_EXPIRES_IN_MINUTES * 60))
+                .sign(Algorithm.HMAC256(Env.JWT_SECRET))
+        } catch (e: Exception) {
+            throw CustomException(
+                statusCode = 500,
+                message = "Could not generate JWT token for user `$userId`",
+                cause = e
+            )
+        }
+
     }
 }
